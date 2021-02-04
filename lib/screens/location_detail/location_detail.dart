349997f05ec9057530756d213dffc479856ade55
Davran Arifzhanov <davron2001davron@gmail.com>
2021-02-04 11:53:23 +0600

    ListView added

--- lib/screens/location_detail/location_detail.dart
+++ lib/screens/location_detail/location_detail.dart
@@ -1,21 +1,29 @@
 import 'package:flutter/material.dart';
 import 'image_banner.dart';
 import 'text_section.dart';
+import '../../models/location.dart';
 
 class LocationDetail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final locations = Location.fetchAll();
+    final location = locations.first;
     return Scaffold(
-        appBar: AppBar(title: Text('Location Detail')),
+        appBar: AppBar(title: Text(location.name)),
         body: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            ImageBanner("assets/images/1.jpg"),
-            TextSection("Summary1", "Sometext1"),
-            TextSection("Summary2", "Sometext2"),
-            TextSection("Summary3", "Sometext"),
-          ],
+            ImageBanner(location.imagePath),
+          ]..addAll(textSections(location)),
         ));
+  }
+
+  //converting TextSections to the LocationFacts
+  //map() doesn't return a list thats's why i'm using toList() method
+  List<Widget> textSections(Location location) {
+    return location.facts
+        .map((fact) => TextSection(fact.title, fact.text))
+        .toList();
   }
 }
